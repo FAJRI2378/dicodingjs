29eeb0bcc8eb0a1a78d71db9dd5ebe5f7eea41ef
FAJRI2378 <armanfajri008@gmail.com>
2025-09-30 13:11:16 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const umur= 17;
+const nama="Feby";
+console.log(`Hai, nama aku ${nama}, umur aku ${umur} tahun.`);
+
+alert(`Hai, nama aku ${nama}, umur aku ${umur} tahun.`);
+
+let name= "arman";
+
+function tampil (){
+    console.log(name);
+}
+tampil();
+console.log(name);
+
+
+function tampilNama(nama){
+    console.log(`Halo, nama saya ${nama}`);
+}
+
+const year =new Date().getFullYear();
+const Text= `Tahun sekarang adalah ${year}`;
